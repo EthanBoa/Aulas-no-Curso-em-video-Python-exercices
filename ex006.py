5cf25f2e1117bc8ce8b510a5186c6de8a7cc592b
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-02-09 15:11:20 +0200

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,8 @@
+n = int(input('Diguite um numero para saber o seu dobro, o seu triplo e a sua raiz quadrada:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O seu dobro e {}, \n O seu triplo e {}, \n A sua raiz quadrada e {}.'.format(d, t, r))
+
+
+

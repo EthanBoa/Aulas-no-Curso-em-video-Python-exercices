71c999cce7e0961b9fb4e01465af5ac599863823
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-02-09 14:35:26 +0200

    Create ex005.py

---
+++ ex005.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero:'))
+n1 = int(input('Quanto antecessores queres?'))
+n2 = int(input('Quntus sucessores qures?'))
+a = n - n1
+s = n + n2
+print('O antecessor de {} e {} e o seu seucessor e {}'.format(n, a, s))

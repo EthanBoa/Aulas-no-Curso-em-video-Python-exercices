2d49e244dfb31a934cafe32535be7b8f2da93ddd
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-02-09 15:26:27 +0200

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,9 @@
+n = str(input('Digite a nota da  1 ACS:'))
+n1 = str(input('Digite a naota da 2 ACS:'))
+n2 = str(input('Digite a nota da APT:'))
+m = (n + n1 + n2) / 3
+print('A sua media e {}'.format(m))
+
+
+
+

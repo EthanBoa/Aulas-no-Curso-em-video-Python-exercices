6113cb5e4b90fd47a3427b16c25cae643f793f0a
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-03-23 14:34:51 +0200

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,5 @@
+l = float(input('Digite a largura da sua parede:'))
+h = float(input("Digite a altura da parede:"))
+a = l * h
+t = a / 2
+print('A sua sua parede tem uma diamensao de {}X{} equivalentes a uma area de {}m2 e e necessario {}l de tinta  '.format(l, h, a, t))

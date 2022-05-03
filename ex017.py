330bb4748e9c9e22a2e880c7b726004993efb017
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-05-03 21:46:21 +0200

    Create ex017.py

---
+++ ex017.py
@@ -0,0 +1,5 @@
+import math
+ca =float(input("Qual e o valor do cateto adjacente:"))
+co = float(input("Qual e o valor do cateto oposto:"))
+h = (ca ** 2 + co ** 2) ** 1/2
+print("O valor da hipotenusa e: {}".format(h))

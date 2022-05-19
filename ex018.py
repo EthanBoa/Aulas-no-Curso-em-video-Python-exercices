f3019bbcfc5f6d84aaffacdad353c41c5ff3869b
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-05-19 17:14:34 +0200

    Update and rename ex0018.py to ex018.py

--- ex018.py
+++ ex018.py
@@ -1,7 +1,7 @@
 import math
-from math import sin, cos, tan
+from math import radians, sin, cos, tan
 a = float(input("Qual e o valor do angulo:"))
-s = sin(math.radians(a))
-c = cos(math.radians(a))
-t = tan(math.radians(a))
+s = sin(radians(a))
+c = cos(radians(a))
+t = tan(radians(a))
 print("O valor do seno e {:.2f} \n O valor do cosseno e {:.2f}\n  O valor da tangente e {:.2f}".format(s, c, t))

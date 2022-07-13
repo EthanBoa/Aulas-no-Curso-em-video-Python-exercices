b72ec62652ec21de31af757c49dacfbf8ea0a8a3
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-13 16:24:26 +0200

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,3 +1,4 @@
+# Exercício Python 023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 n = int(input('Diguite um numero :'))
 u = n // 1 % 10
 d = n // 10 % 10

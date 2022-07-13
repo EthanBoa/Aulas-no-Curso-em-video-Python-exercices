f7f3b190562d8ac6d144e103ecf672b6340b5f7e
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-13 16:41:00 +0200

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,3 +1,4 @@
+#Exercício Python 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 c = float(input('Qual e a temperatura em °C?'))
 t = c * 1.8 + 32
 print('A sua tempratura em °F e de {}°F'.format(t))

05b338e530e19a1edbd4b26dc8eca4d9ed763a3f
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-13 16:26:04 +0200

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,3 +1,7 @@
+# Exercício Python 022: Crie um programa que leia o nome completo de uma pessoa e mostre: 
+#- O nome com todas as letras maiúsculas e minúsculas.
+#- Quantas letras ao todo (sem considerar espaços).
+#- Quantas letras tem o primeiro nome.
 n = str(input("Qual e o seu nome?")).strip()
 print("O seu nome com todas letras maisculas fica:", n.upper())
 print("O seu nome com todas letras menusculas fica:", n.lower())

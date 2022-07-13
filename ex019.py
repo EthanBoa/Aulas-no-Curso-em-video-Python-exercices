a86649f1f0c36d4541fb75e6abde655f7b78b5b4
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-13 16:36:37 +0200

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -1,3 +1,4 @@
+# Exercício Python 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 from random import choice
 a = input("Qual o nome da primeiro aluno:")
 b = input("Qual o nome da segundo aluno:")

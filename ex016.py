46799ac4d12b473a535121d3c0d29f05e3e3963a
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-13 16:39:20 +0200

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,3 +1,4 @@
+# Exercício Python 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 from math import trunc
 n = float(input("Digite um numero:"))
 i = trunc(n)

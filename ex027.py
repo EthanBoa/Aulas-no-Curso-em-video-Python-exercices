c09a0109f3f0b5bc3f94a706554575243eacbf70
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2022-07-25 17:30:41 +0200

    Create ex027.py

---
+++ ex027.py
@@ -0,0 +1,5 @@
+n = str(input("Qual e o seu nome?")).strip()
+m = n.split()
+print('Muito prazer e te conhecer!')
+print("O seu primero nome e {}".format(m[0]))
+print("O seu ultimo nome e {}".format(m[len(m)-1]))

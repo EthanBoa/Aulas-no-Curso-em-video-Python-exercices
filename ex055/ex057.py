16fd9fa750473bea6684b4e1f8d7052ded72527a
Ethanboa <96380575+EthanBoa@users.noreply.github.com>
2024-08-07 14:10:12 +0200

    Delete ex055 directory

--- ex055/ex057.py
+++
@@ -1,8 +0,0 @@
-#Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
-
-
-s = str(input(" Digite o seu sexo [ M/F ]:")).strip().upper()[0]
-while s not in 'M F':
-    s = str(input("Dados invalido. Por favor digite o seu sexo:")).strip().upper()[0]
-
-print("Sexo {} digitado com sucesso.".format(s))
